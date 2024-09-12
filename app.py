047e6a5cae0f68e825e720959b0649957954df54
devwenderson <wend.devdesigner@gmail.com>
2024-09-11 21:23:15 -0300

    adiciona visualização do boletim

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, redirect, url_for, session, request, jsonify, render_template
+from authlib.integrations.flask_client import OAuth
+from views.main import oauthRegister, User
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = 'development'
+oauth = OAuth(app)
+oauthRegister(oauth, 
+              name = "suap", 
+              api_base_url="https://suap.ifrn.edu.br/api/", 
+              request_token_url=None, 
+              access_token_method="POST", 
+              access_token_url="https://suap.ifrn.edu.br/o/token/", 
+              authorize_url="https://suap.ifrn.edu.br/o/authorize/", token="suap_token")
+
+@app.route('/')
+def index():
+    if 'suap_token' in session:
+        user = User(oauth)
+        user_data = user.fetchUserDados()
+        data = {"user_data": user_data.json()}
+        return render_template('user.html', data=data)
+    else:
+        return render_template('index.html')
+    
+@app.route("/boletim", methods=["GET"])
+def boletim():
+    user = User(oauth)
+    user_data = user.fetchUserDados()
+    data = {"user_data": user_data.json()}
+    if (request.args.get('ano_letivo')):
+        ano_letivo = int(request.args.get('ano_letivo'))
+        boletim = user.fetchUserBoletim(ano_letivo)
+        data["boletim"] = boletim.json()
+        
+    return render_template("boletim.html", data=data)
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.suap.authorize_redirect(redirect_uri)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('suap_token', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/login/authorized')
+def auth():
+    token = oauth.suap.authorize_access_token()
+    session['suap_token'] = token
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run()
+    
+    
